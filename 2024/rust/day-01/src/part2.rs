3b11e6ac87a746888630633c73ea729d31c98183
BritishWerewolf <britishwerewolfdev@gmail.com>
2024-12-02 21:43:02 +0000

    Part 2 complete.

--- 2024/rust/day-01/src/part2.rs
+++ 2024/rust/day-01/src/part2.rs
@@ -1,7 +1,21 @@
 pub fn process(input: &str) -> u32 {
     let _input = input.replace("\r\n", "\n");
 
-    0
+    let mut left = Vec::<u32>::new();
+    let mut right = Vec::<u32>::new();
+
+    input
+        .lines()
+        .for_each(|line| {
+            let mut splits = line.split_whitespace();
+            left.push(splits.next().unwrap_or_default().parse::<u32>().unwrap_or_default());
+            right.push(splits.next().unwrap_or_default().parse::<u32>().unwrap_or_default());
+        });
+
+    left
+        .into_iter()
+        .map(|l| l * u32::try_from(right.iter().filter(|&&r| l == r).count()).unwrap_or_default())
+        .sum()
 }
 
 #[cfg(test)]
@@ -10,17 +24,21 @@ mod tests {
 
     #[test]
     fn example_input() {
-        let input = "";
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
         let result = process(&input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 31);
     }
 
-    #[ignore]
     #[test]
     fn real_answer() {
         let input = std::env::current_dir().unwrap().display().to_string() + "/src/input.txt";
         let input = std::fs::read_to_string(input).expect("input to exist");
         let result = process(&input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 21790168);
     }
 }
