4bda363ec417cbb3aac1216578a7d96f19cd96dd
BritishWerewolf <britishwerewolfdev@gmail.com>
2024-12-07 23:29:14 +0000

    Part 1 complete.

--- 2024/rust/day-03/src/part1.rs
+++ 2024/rust/day-03/src/part1.rs
@@ -1,7 +1,47 @@
+use nom::{bytes::complete::tag, character::complete::{anychar, digit1}, combinator::map_res, multi::{many0, many_till}, sequence::{delimited, separated_pair}, IResult, Parser};
+
+#[derive(Clone, Copy, Debug, Default)]
+struct Pair {
+    left: u32,
+    right: u32,
+}
+
+impl Pair {
+    fn mul(&self) -> u32 {
+        self.left * self.right
+    }
+}
+
+fn parse_pair(input: &str) -> IResult<&str, Pair> {
+    delimited(
+        tag("mul("),
+        map_res(
+            separated_pair(digit1, tag(","), digit1),
+            |(left, right): (&str, &str)| -> Result<Pair, std::num::ParseIntError> {
+                Ok(Pair {
+                    left: left.parse::<u32>()?,
+                    right: right.parse::<u32>()?
+                })
+            }
+        ),
+        tag(")")
+    )(input)
+}
+
+fn parse_pairs(input: &str) -> IResult<&str, Vec<Pair>> {
+    many0(
+        many_till(
+            anychar,
+            parse_pair
+        ).map(|(_discard, pair)| pair)
+    )(input)
+}
+
 pub fn process(input: &str) -> u32 {
     let _input = input.replace("\r\n", "\n");
+    let (_, pairs) = parse_pairs(input).unwrap_or_default();
 
-    0
+    pairs.into_iter().map(|pair| pair.mul()).sum()
 }
 
 #[cfg(test)]
@@ -15,12 +55,11 @@ mod tests {
         assert_eq!(result, 161);
     }
 
-    #[ignore]
     #[test]
     fn real_answer() {
         let input = std::env::current_dir().unwrap().display().to_string() + "/src/input.txt";
         let input = std::fs::read_to_string(input).expect("input to exist");
         let result = process(&input);
-        assert_eq!(result, 0);
+        assert_eq!(result, 153469856);
     }
 }
